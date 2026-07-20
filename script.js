6e3b7005f5d2a30518241eefcb1f5974bbd6f6b9
caramel-frappe <nh29_chua_xin_ying_claire@nhhs.edu.sg>
2026-07-20 09:29:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+
+// Echoes in the Hall
+// Part 1A
+
 console.log("Echoes in the Hall loaded!");
+
+const playButton = document.getElementById("playButton");
+const menu = document.getElementById("menu");
+const hud = document.getElementById("hud");
+const loading = document.getElementById("loading");
+
+playButton.addEventListener("click", () => {
+    menu.style.display = "none";
+    hud.style.display = "block";
+    loading.textContent = "Entering the school...";
+});
